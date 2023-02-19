9420963197c4a97a55bfeeac279455f6d6e46b63
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-02-19 11:37:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the standard and makes an error 
+ *Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
+
+
