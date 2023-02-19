1b70537dd3dcc69c6f248f2324a92722715f7fa7
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-02-19 10:23:12 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main -A program that prits a line using printf function 
+ * Returns 0 (Success)
+ */
+int main(void)
+{
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
