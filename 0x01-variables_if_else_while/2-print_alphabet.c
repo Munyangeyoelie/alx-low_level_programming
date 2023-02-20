be45a98c3dff180552a2c8251342a4db36892f9c
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-02-19 16:00:20 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <studio>
+/**
+ * main - Entry point
+ * Return: Always  0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while 
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
