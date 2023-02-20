9f0780694b0b32095729e18ad1a3ab4ae5ae05bd
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-02-20 03:31:21 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low, e, q;
+	e = 'e';
+
+
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && != q)
+		putchar('\n');
+		return (0);
+	}
+}
