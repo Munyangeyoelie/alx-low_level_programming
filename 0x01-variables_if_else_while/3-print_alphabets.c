edd05e4015135e76d9a26b20ef48a8f6d03015dc
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-02-20 02:56:01 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'A'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
