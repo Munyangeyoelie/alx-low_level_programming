9e3ad2a87243382c5b9573f4db894d0f24afa22f
Munyangeyoelie <munyangeyoelie38@gmail.com>
2023-06-29 06:36:57 -0700

    changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,61 +1,38 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- *_strlen - reset number
- *Description: This function return a length for some string
- *@s: pointer char
- *Return: int length
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
-
-int _strlen(char *s)
+char *cap_string(char *str)
 {
-	int len = 0;
+	int index = 0;
 
-	while (*s++)
+	while (str[index])
 	{
-		len++;
-	}
-	return (len);
-}
-/**
- *cap_string - changes all lowercase letters
- *@s1: pointer parameter"
- *Description: changes all lowercase letters
- *Return: return pointer
- */
-char *cap_string(char *s1)
-{
-	int i, j;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-	for (i = 0; i < _strlen(s1) - 1; i++)
-	{
-		if (
-			s1[i] == ' ' ||
-			s1[i] == '\t' ||
-			s1[i] == '\n' ||
-			s1[i] == ',' ||
-			s1[i] == ';' ||
-			s1[i] == '.' ||
-			s1[i] == '!' ||
-			s1[i] == '?' ||
-			s1[i] == '"' ||
-			s1[i] == '(' ||
-			s1[i] == ')' ||
-			s1[i] == '{' ||
-			s1[i] == '}' ||
-			i == 0
-		)
-		{
-			for (j = 'a'; j <= 'z'; j++)
-			{
-				if (s1[i + 1] == j && i != 0)
-				{
-					s1[i + 1] = j - 32;
-				} else if (s1[i] == j && i == 0)
-				{
-					s1[i] = j - 32;
-				}
-			}
-		}
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (s1);
-}
+
+	return (str);
+
